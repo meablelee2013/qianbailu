f43af8a465fc322ddd6008ad112b52c66e240826
fengmnegchang <624926379@qq.com>
2017-01-11 17:06:14 +0800

    删除默认信息 Signed-off-by: fengmnegchang <624926379@qq.com>

--- src/com/open/qianbailu/fragment/PCQianBaiLuMovieDetailFragment.java
+++ src/com/open/qianbailu/fragment/PCQianBaiLuMovieDetailFragment.java
@@ -43,15 +43,15 @@ public class PCQianBaiLuMovieDetailFragment extends QianBaiLuMMovieDetailFragmen
 		return fragment;
 	}
 
-	/* (non-Javadoc)
-	 * @see android.support.v4.app.Fragment#onViewCreated(android.view.View, android.os.Bundle)
-	 */
-	@Override
-	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onViewCreated(view, savedInstanceState);
-		url = UrlUtils.PC_QIAN_BAI_LU_MOVIE;
-	}
+//	/* (non-Javadoc)
+//	 * @see android.support.v4.app.Fragment#onViewCreated(android.view.View, android.os.Bundle)
+//	 */
+//	@Override
+//	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+//		// TODO Auto-generated method stub
+//		super.onViewCreated(view, savedInstanceState);
+//		url = UrlUtils.PC_QIAN_BAI_LU_MOVIE;
+//	}
 
 	/*
 	 * (non-Javadoc)
